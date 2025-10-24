cd020da3f2f59d39a343f269c644f935ed247ae4
HenryZisow <28hzisow@gannacademy.org>
2025-10-23 22:36:10 -0400

    added comments

--- src/App.java
+++ src/App.java
@@ -2,7 +2,6 @@ import processing.core.*;
 
 public class App extends PApplet {
 
-    float uselsss;
     float r = 1; //color of rectangles
     float g = 1; //color of rectangles
     float b = 1; //color of rectangles
